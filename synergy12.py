bd78f6dc7e5fcd2840afafbb88501fbb293c9531
Belosnejka <153391631+Belosnejechka@users.noreply.github.com>
2025-01-07 21:44:19 +0300

    Update synergy12.py
    debug

--- synergy12.py
+++ synergy12.py
@@ -1,9 +1,8 @@
 str = input()
 x = 0
-while x < len(str):
+while x+1 < len(str):
     if (str[x] == ' ') and (str[x+1] == ' '):
         str = str[0:x] + str[x+1:len(str)]
-        x += 1
         print(str)
     else:
         x += 1
